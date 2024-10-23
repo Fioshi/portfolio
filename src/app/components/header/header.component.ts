53ed52e5f5436ecd661bb5a51a4eac5af7fd4a6e
Fioshi <guilhermefiochi@outlook.com>
2024-10-23 10:40:55 -0300

    Adicionados botões responsivos no header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 @Component({
@@ -8,7 +11,9 @@ import {MatButtonModule} from '@angular/material/button';
   standalone: true,
   imports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
