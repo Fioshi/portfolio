aa09b777260fa764072374addb94973fc84add1b
Fioshi <guilhermefiochi@outlook.com>
2024-10-25 09:19:57 -0300

    Projeto inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { AboutComponent } from "./components/about/about.component";
 import { TecnlogiesComponent } from "./components/tecnlogies/tecnlogies.component";
 import { ProjectsComponent } from "./components/projects/projects.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, TecnlogiesComponent, ProjectsComponent, RouterModule],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, TecnlogiesComponent, ProjectsComponent, RouterModule, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { routes } from '../../app.routes';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+
+
+}
